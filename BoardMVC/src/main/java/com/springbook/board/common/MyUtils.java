acbb1f2475087ea9f1c40cb433d25234e5625477
sbsteacher <pirbak@daum.net>
2020-07-24 16:51:48 +0900

    회원가입 작업중_2

--- BoardMVC/src/main/java/com/springbook/board/common/MyUtils.java
+++ BoardMVC/src/main/java/com/springbook/board/common/MyUtils.java
@@ -1,5 +1,9 @@
 package com.springbook.board.common;
 
-public class MyUtils {
+import org.mindrot.jbcrypt.BCrypt;
 
+public class MyUtils {
+	public static String hashPassword(String pw) {
+		return BCrypt.hashpw(pw, BCrypt.gensalt());
+	}
 }
--- BoardMVC/src/main/java/com/springbook/board/user/UserService.java
+++ BoardMVC/src/main/java/com/springbook/board/user/UserService.java
@@ -1,13 +1,18 @@
 package com.springbook.board.user;
 
 import org.springframework.stereotype.Service;
+import com.springbook.board.common.MyUtils;
 
 @Service
 public class UserService {
 	
 	public int join(UserVO param) {
 		int result = 0;
+		String pw = param.getUpw();
+		String hashPw = MyUtils.hashPassword(pw);		
+		param.setUpw(hashPw);
 		
+		//param.setUpw(MyUtils.hashPassword(param.getUpw()));
 		return result;
 	}
 }
