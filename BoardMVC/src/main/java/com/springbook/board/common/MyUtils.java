1e6c106a3314286cc26e18dcb32f4e2b0e823c1b
sbsteacher <pirbak@daum.net>
2020-07-24 16:42:13 +0900

    회원가입 작업중

---
+++ BoardMVC/src/main/java/com/springbook/board/common/MyUtils.java
@@ -0,0 +1,5 @@
+package com.springbook.board.common;
+
+public class MyUtils {
+
+}
---
+++ BoardMVC/src/main/java/com/springbook/board/user/UserService.java
@@ -0,0 +1,13 @@
+package com.springbook.board.user;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+	
+	public int join(UserVO param) {
+		int result = 0;
+		
+		return result;
+	}
+}
