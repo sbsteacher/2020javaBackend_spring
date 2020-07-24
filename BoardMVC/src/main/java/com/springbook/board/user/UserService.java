1998190ea960cc3598c1c5b9e659d6edf1666d5d
sbsteacher <pirbak@daum.net>
2020-07-24 17:46:20 +0900

    회원가입 완료

--- BoardMVC/src/main/java/com/springbook/board/user/UserService.java
+++ BoardMVC/src/main/java/com/springbook/board/user/UserService.java
@@ -1,18 +1,24 @@
 package com.springbook.board.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
 import com.springbook.board.common.MyUtils;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private UserMapper mapper;
 	
 	public int join(UserVO param) {
 		int result = 0;
 		String pw = param.getUpw();
 		String hashPw = MyUtils.hashPassword(pw);		
 		param.setUpw(hashPw);
-		
 		//param.setUpw(MyUtils.hashPassword(param.getUpw()));
+				
+		result = mapper.join(param);
 		return result;
 	}
 }
